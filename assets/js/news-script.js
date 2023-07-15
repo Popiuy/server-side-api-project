b5a402da2e77981d1d42befc0fc50af263adc09a
Dash <dkshowarth@gmail.com>
2023-07-15 15:05:11 -0700

    add function for creating entries on page from api call

--- assets/js/news-script.js
+++ assets/js/news-script.js
@@ -1,13 +1,23 @@
 
 //section for api management 
-var url = 'https://api.nytimes.com/svc/search/v2/articlesearch.json?q=obama&api-key=Eulq1k1mu9GVwfHXAOmphojyaTQWGIGu'
+var resultsDisplay = $('#resultsDisplay')
+console.log(resultsDisplay)
+var url = 'https://api.nytimes.com/svc/search/v2/articlesearch.json?q=obama+ukraine&api-key=Eulq1k1mu9GVwfHXAOmphojyaTQWGIGu'
 
 fetch(url)
 
 .then(function(response){
-    console.log(response);
-    response = response.json();
-    console.log(response);
+    return response.json();
+})
+.then(function(data){
+    console.log(data.response);
+    //create new a item, make the content the abstract, and add an href
+    for(var i = 0; i < data.response.docs.length; i++){
+        var newEntry = $('<div></div>');
+        // newEntry.attr('href', data.response.docs[i]['web_url']);
+        newEntry.text(data.response.docs[i]['abstract']);
+        resultsDisplay.append(newEntry);
+    }
 })
 
 //section for timer - on page load, timer starts counting for the page you're on, timer for other page 
