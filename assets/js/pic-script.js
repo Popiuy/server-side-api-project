887843ff5f36edad86bdbae0f70c047207dcbab8
Popiuy <rymaxpayne@gmail.com>
2023-07-15 22:25:41 -0700

    added functionality to cat js

--- assets/js/pic-script.js
+++ assets/js/pic-script.js
@@ -1,15 +1,58 @@
 // section for api management
-var picsUrl = 'https://api.artic.edu/api/v1/artworks/search?q=cats&query[term][is_public_domain]=true'
+var picsUrl = 'https://api.artic.edu/api/v1/artworks/search?q=cats&query[term][is_public_domain]=true';
 
+var input = document.querySelector("#input");
+var form = document.querySelector("#form");
+var resultsDisplay = document.querySelector("#resultsdisplay"); // Add resultsdisplay element
+
+// fetch call to return information from the API
 fetch(picsUrl)
+  .then(function(response) {
+    return response.json();
+  })
+  .then(function(data) {
+    renderCatData(data);
+  })
+  .catch(function(error) {
+    console.log("Error:", error);
+  });
 
-.then(function(response){
-    console.log(response);
-    response = response.json();
-    console.log(response);
-})
+// event listener to check what the user is interested in
+form.addEventListener("submit", function(event) {
+  event.preventDefault();
+  var searchInput = input.value.trim(); // Change input.data.trim() to input.value.trim()
+  if (searchInput !== "") {
+    fetchData(searchInput);
+  } else {
+    alert("Please enter types of cats you'd like to see");
+  }
+});
 
-chroma({ h:120, s:1, l:0.75});
+// function to handle the information we returned and append api_link to the page
+function renderCatData(data) {
+  console.log(data);
+  var catList = data.data;
+  for (var i = 0; i < catList.length; i++) {
+    var currentCatData = catList[i];
+    var catDataEl = document.createElement("div");
+    catDataEl.textContent = currentCatData.api_link;
+    resultsDisplay.appendChild(catDataEl);
+  }
+}
+
+function fetchData(searchInput) {
+  var searchUrl = 'https://api.artic.edu/api/v1/artworks/search?q=' + searchInput + '&query[term][is_public_domain]=true';
+  fetch(searchUrl)
+    .then(function(response) {
+      return response.json();
+    })
+    .then(function(data) {
+      renderCatData(data);
+    })
+    .catch(function(error) {
+      console.log("Error:", error);
+    });
+}
 //section for pagination
 
 // section for timers
