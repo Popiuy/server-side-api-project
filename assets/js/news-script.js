d900cd1ad31ee8457aaf652c935b6656ae330709
Dash <dkshowarth@gmail.com>
2023-07-16 11:48:54 -0700

    add comments to news js

--- assets/js/news-script.js
+++ assets/js/news-script.js
@@ -5,16 +5,22 @@ var cardTemplate = $('#entryCard');
 var searchField = $('#searchInputField');
 var searchTerm = '';
 
+// Creating new cards for articles and 
 var createEntries = function(apiData){
     for(var i = 0; i < apiData.response.docs.length; i++){
+        //clones card element from html
         var newEntry = cardTemplate.clone();
+        //add info from article
         newEntry.find('p').text(apiData.response.docs[i]['abstract'])
         newEntry.find('a').attr('href', apiData.response.docs[i]['web_url']);
+        newEntry.find('h3').text(apiData.response.docs[i].headline.main)
+        //remove hidden tag from cloned card element
         newEntry.attr('style','');
         resultsDisplay.append(newEntry);
     }
 }
 
+//Function for grabbing search terms and printing the results to screen
 var searchDisplay = function(searchInput){
 
     var url = 'https://api.nytimes.com/svc/search/v2/articlesearch.json?q=' + searchInput + '&api-key=Eulq1k1mu9GVwfHXAOmphojyaTQWGIGu'
@@ -24,17 +30,19 @@ var searchDisplay = function(searchInput){
     .then(function(response){
         return response.json();
     })
+
     .then(function(data){
         console.log(data.response);
         //create new a item, make the content the abstract, and add an href
         createEntries(data);
     })
-
-
 }
 
+//listener for search field
 searchField.submit(function(event){
     event.preventDefault();
+    //clear previous search results
+    resultsDisplay.html('');
     searchTerm = $('#search').val();
     $('#search').val('');
     searchDisplay(searchTerm);
