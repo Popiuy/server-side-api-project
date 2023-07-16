2c1036dac02fdf07425dea03608fe2180a239de0
TCodes <theodoreuokeke@gmail.com>
2023-07-16 16:40:22 +0100

    selected elements and created new variables

---
+++ assets/js/timer.js
@@ -0,0 +1,18 @@
+// select all necessary elements
+var timeDisplay = document.querySelector('#time-display');
+var newsBtn = document.querySelector('#news-btn');
+var catsBtn = document.querySelector('#cats-btn');
+
+//assign necessary variables
+var startTime = 0;
+var pastTime = 0;
+var currentTime = 0;
+var paused = true;
+var timerInterval;
+var hours = 0;
+var minutes = 0;
+var seconds = 0;
+
+
+
+
