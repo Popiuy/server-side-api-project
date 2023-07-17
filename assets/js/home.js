ae9c2c03a4743cb2aece00e9726f2e27c52124fa
Dash <dkshowarth@gmail.com>
2023-07-16 18:08:50 -0700

    add localStorage variables for timers to home page

--- assets/js/home.js
+++ assets/js/home.js
@@ -1,3 +1,11 @@
+
+store.set('newsTime', 0);
+store.set('catTime', 0);
+
 $('#happy').on('click', function(event){
     window.location = './assets/html/pics-results.html'
+})
+
+$('#informed').on('click', function(event){
+    window.location = './assets/html/news-results.html'
 })
